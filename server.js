63a072ee98d0ebadddb2743979ee1aa824669d37
Baiaman Bazarbaev <baiamansama@gmail.com>
2021-08-03 17:08:58 +0900

    JWT

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express()
 connectDB()
 const PORT = process.env.PORT || 5000
 
+app.use(express.json({ extended: false }))
 app.get('/', (req, res) => res.send('API Running'))
+
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profiles', require('./routes/api/profiles'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
