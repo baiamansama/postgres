863f00018ad695dfe35a5e574b53b3cbdc4b3332
Baiaman Bazarbaev <baiamansama@gmail.com>
2021-08-04 14:55:53 +0900

    back front

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => res.send('API Running'))
 
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/profiles', require('./routes/api/profiles'))
+app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
